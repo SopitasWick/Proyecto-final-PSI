140fff15a54c137e63f4b40d21893e3acc00c7ed
SopitasWick <jesus.izaguirre216660@potros.itson.edu.mx>
2024-11-02 12:51:10 -0700

    Primer commit

---
+++ Dominio/src/main/java/itson/com/mx/dominio/Dominio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package itson.com.mx.dominio;
+
+/**
+ *
+ * @author usuario
+ */
+public class Dominio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
