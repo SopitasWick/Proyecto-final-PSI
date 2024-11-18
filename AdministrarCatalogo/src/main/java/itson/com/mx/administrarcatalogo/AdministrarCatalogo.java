b3d7a100a20c6834c11c74dd2c88a5c590704618
SopitasWick <jesus.izaguirre216660@potros.itson.edu.mx>
2024-11-17 17:39:04 -0700

    Se comento la clase de prueba de administrar catalogo

--- AdministrarCatalogo/src/main/java/itson/com/mx/administrarcatalogo/AdministrarCatalogo.java
+++ AdministrarCatalogo/src/main/java/itson/com/mx/administrarcatalogo/AdministrarCatalogo.java
@@ -15,37 +15,37 @@ public class AdministrarCatalogo {
 
     public static void main(String[] args) {
        // System.out.println("Hello World!");
-       Administrar ad= new Administrar();
-       System.out.println("Pruebas de la implementacion Adminstrar()");
-        try {
-            System.out.println("***Prueba RegistrarCarrito");
-            ad.RegistrarCarrito(new Carrito(181, 1900, "nuevo", "bueno", "ocupado", new byte[3]));
-            ad.RegistrarCarrito(new Carrito(111, 2300, "viejo", "regular", "ocupado", new byte[3]));
-            ad.RegistrarCarrito(new Carrito(121, 2000, "comprado", "libre", "ocupado", new byte[3]));
-            ad.RegistrarCarrito(new Carrito(131, 1500, "azul", "nuevo", "libre", new byte[3]));
-            ad.RegistrarCarrito(new Carrito(141, 2340, "limpio", "nuevo", "libre", new byte[3]));
-            ad.RegistrarCarrito(new Carrito(151, 1500, "no funciona", "libre", "ocupado", new byte[3]));
-            ad.RegistrarCarrito(new Carrito(161, 3000, "gringo", "excelente", "libre", new byte[3]));
-            ad.RegistrarCarrito(new Carrito(171, 1000, "no sirve", "nuevo", "libre", new byte[3]));
-            System.out.println("ConsultarCarritoId");
-            System.out.println(ad.ConsultarCarritoId(171));
-            System.out.println(ad.ConsultarCarritoId(131));
-            System.out.println(ad.ConsultarCarritoId(141));
-            System.out.println(ad.ConsultarCarritoId(161));
-            System.out.println(ad.ConsultarCarritoId(111));
-            System.out.println("ConsultarCatalogoCarritos");
-            ad.ConsultarCatalogoCarritos().forEach((n) -> System.out.println(n));
-            System.out.println("ActualizarCarrito");
-            ad.ActualizarCarrito(new Carrito(141, 0, "owo", "", "", new byte[3]));
-            ad.ActualizarCarrito(new Carrito(171, 0, "ewe", "", "", new byte[3]));
-            System.out.println(ad.ConsultarCarritoId(141));
-            System.out.println(ad.ConsultarCarritoId(171));
-            System.out.println("EliminarCarrito");
-            ad.EliminarCarrito(141);
-            ad.EliminarCarrito(171);
-            ad.ConsultarCatalogoCarritos().forEach((n) -> System.out.println(n));
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+//       Administrar ad= new Administrar();
+//       System.out.println("Pruebas de la implementacion Adminstrar()");
+//        try {
+//            System.out.println("***Prueba RegistrarCarrito");
+//            ad.RegistrarCarrito(new Carrito(181, 1900, "nuevo", "bueno", "ocupado", new byte[3]));
+//            ad.RegistrarCarrito(new Carrito(111, 2300, "viejo", "regular", "ocupado", new byte[3]));
+//            ad.RegistrarCarrito(new Carrito(121, 2000, "comprado", "libre", "ocupado", new byte[3]));
+//            ad.RegistrarCarrito(new Carrito(131, 1500, "azul", "nuevo", "libre", new byte[3]));
+//            ad.RegistrarCarrito(new Carrito(141, 2340, "limpio", "nuevo", "libre", new byte[3]));
+//            ad.RegistrarCarrito(new Carrito(151, 1500, "no funciona", "libre", "ocupado", new byte[3]));
+//            ad.RegistrarCarrito(new Carrito(161, 3000, "gringo", "excelente", "libre", new byte[3]));
+//            ad.RegistrarCarrito(new Carrito(171, 1000, "no sirve", "nuevo", "libre", new byte[3]));
+//            System.out.println("ConsultarCarritoId");
+//            System.out.println(ad.ConsultarCarritoId(171));
+//            System.out.println(ad.ConsultarCarritoId(131));
+//            System.out.println(ad.ConsultarCarritoId(141));
+//            System.out.println(ad.ConsultarCarritoId(161));
+//            System.out.println(ad.ConsultarCarritoId(111));
+//            System.out.println("ConsultarCatalogoCarritos");
+//            ad.ConsultarCatalogoCarritos().forEach((n) -> System.out.println(n));
+//            System.out.println("ActualizarCarrito");
+//            ad.ActualizarCarrito(new Carrito(141, 0, "owo", "", "", new byte[3]));
+//            ad.ActualizarCarrito(new Carrito(171, 0, "ewe", "", "", new byte[3]));
+//            System.out.println(ad.ConsultarCarritoId(141));
+//            System.out.println(ad.ConsultarCarritoId(171));
+//            System.out.println("EliminarCarrito");
+//            ad.EliminarCarrito(141);
+//            ad.EliminarCarrito(171);
+//            ad.ConsultarCatalogoCarritos().forEach((n) -> System.out.println(n));
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//        }
     }
 }
